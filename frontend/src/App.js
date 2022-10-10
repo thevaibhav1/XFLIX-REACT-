eff3fca39cad8d2d08c6aa61e06b6fde816771a8
CrioUser <crio-user@criodo.com>
2022-10-10 20:11:43 +0530

    ME_BUILDOUT_XFLIX_REACT_MODULE_REACT

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import VideoGrid from "./components/VideoGrid";
+import VideoDetails from "./components/VideoDetails";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+
+export const config = {
+  endpoint: `https://07ddcc16-d0ab-4dec-b0f2-63562ece73b1.mock.pstmn.io/v1`,
+};
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path="/" component={VideoGrid}/>
+        <Route exact path="/video/:id" component={VideoDetails}/>
+      </Switch>
     </div>
   );
 }
 
 export default App;
+
+
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { SnackbarProvider } from "notistack";
+import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <SnackbarProvider
+          maxSnack={1}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "center",
+          }}
+          preventDuplicate
+        >
+          <App />
+        </SnackbarProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
